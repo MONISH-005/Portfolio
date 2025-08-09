dbbafa615928748a1f92f8e0b478c6e85409b15b
Monish-005 <monishnarasimman2005@gmail.com>
2025-08-09 22:51:33 +0530

    modify

--- app.js
+++ app.js
@@ -49,3 +49,14 @@ document.getElementById('contactForm').addEventListener('submit', function(e) {
         responseMessage.style.color = "red";
     });
 });
+
+
+
+
+
+ 
+  function toggleMenu() {
+    const nav = document.getElementById("nav-menu");
+    nav.classList.toggle("show");
+  }
+ 
